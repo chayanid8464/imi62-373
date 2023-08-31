f5a7e033684ffae08389ab3562b2114c9338c781
unknown <chayanidkp41@gmail.com>
2023-08-31 18:31:18 +0700

    yeah this is commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'screen/home.dart';
+import 'screen/login.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Users CRUD',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const Home(),
+        '/login': (context) => const Login(),
+      },
+    );
+  }
+}
---
+++ lib/models/config.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_application_20/models/users.dart';
+
+class Configure {
+  static const server = "10.116.1.250:3000";
+  static Users login = Users();
+  static List<String> gender = ["None", "Male", "Female"];
+}
